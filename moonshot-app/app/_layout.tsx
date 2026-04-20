5c305bcce1c7d8d7b212e5d1f522c9fef08f24ee
Jaycey <jayceybrugge23@gmail.com>
2026-04-20 12:04:47 +0200

    added custom font to the app

--- moonshot-app/app/_layout.tsx
+++ moonshot-app/app/_layout.tsx
@@ -1,9 +1,49 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import { HeroUINativeProvider } from "heroui-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import '../globals.css';
+import { 
+  Lexend_100Thin,
+  Lexend_200ExtraLight,
+  Lexend_300Light,
+  Lexend_400Regular, 
+  Lexend_500Medium,
+  Lexend_600SemiBold,
+  Lexend_700Bold,
+  Lexend_800ExtraBold,
+  Lexend_900Black
+} from "@expo-google-fonts/lexend";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Lexend-Thin": Lexend_100Thin,
+    "Lexend-ExtraLight": Lexend_200ExtraLight,
+    "Lexend-Light": Lexend_300Light,
+    "Lexend-Regular": Lexend_400Regular,
+    "Lexend-Medium": Lexend_500Medium,
+    "Lexend-SemiBold": Lexend_600SemiBold,
+    "Lexend-Bold": Lexend_700Bold,
+    "Lexend-ExtraBold": Lexend_800ExtraBold,
+    "Lexend-Black": Lexend_900Black,
+  })
+
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync().catch(() => { });
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync().catch(() => { });
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <HeroUINativeProvider>
